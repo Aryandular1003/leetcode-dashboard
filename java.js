31865402785ff4163634cefd1cc9418da6dc6dbb
Aryan Kumar <167390317+Aryandular1003@users.noreply.github.com>
2026-08-01 10:04:19 +0530

    Update java.js

--- java.js
+++ java.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded",function(){
     }
         
    async function fenchUserDetails(username) {
-    const url = `https://alfa-leetcode-api.onrender.com/:${username}`;
+    const url = `https://leetcode-stats-api.herokuapp.com/${username}`;
 
     try {
         statsContainer.style.display = "none"; 
