52b2fe74e34fd6f90669b65f5637b6f161d17fc4
Aryan Kumar <167390317+Aryandular1003@users.noreply.github.com>
2026-08-01 10:10:20 +0530

    Update user details fetch URL and fix typos

--- java.js
+++ java.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded",function(){
     }
         
    async function fenchUserDetails(username) {
-    const url = `https://leetcode-stats-api.herokuapp.com/${username}`;
+    const url = `https://leetcode-stats.tashif.codes/${username}/profile`;
 
     try {
         statsContainer.style.display = "none"; 
