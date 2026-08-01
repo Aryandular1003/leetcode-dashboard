bffe8556e43ad38f56a63a701221a6638ffc5028
Aryan Kumar <167390317+Aryandular1003@users.noreply.github.com>
2026-08-01 09:59:44 +0530

    Update API endpoint for fetching user details

--- java.js
+++ java.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded",function(){
     }
         
    async function fenchUserDetails(username) {
-    const url = `https://leetcode-stats-api.herokuapp.com/${username}`;
+    const url = `https://alfa-leetcode-api.onrender.com/${username}/profile`;
 
     try {
         statsContainer.style.display = "none"; 
